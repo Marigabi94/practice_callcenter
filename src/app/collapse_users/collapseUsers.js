dbf07b7bd4e15666cab83e2c071fc1a15c348ff1
Marigabi94 <marigabi940@gmail.com>
2022-02-15 18:20:55 -0400

    creacion de data de prueba

---
+++ src/app/collapse_users/collapseUsers.js
@@ -0,0 +1,27 @@
+import usersData from "./DataUsers.js";
+
+
+/* 
+
+inyection data in button: 
+        name,
+        apellido,
+
+status tlf: 'icon in button modificar'
+status agente : 'color button modificar'
+
+
+inyection data in card collapse:
+        ext,
+        in,
+        out,
+        nivel,
+        grupo,
+        localizacion,
+        ultimo estado
+
+
+*/
+
+
+
