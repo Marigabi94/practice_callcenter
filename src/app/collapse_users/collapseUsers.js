049411bf50c2dce08b1994f491b37ee283196a61
Marigabi94 <marigabi940@gmail.com>
2022-02-16 21:48:32 -0400

    Diseño terminado. Solo falta funcion de filtrado.

--- src/app/collapse_users/collapseUsers.js
+++ src/app/collapse_users/collapseUsers.js
@@ -2,17 +2,7 @@ import {usersData} from './DataUsers.js';
 /* 
 
 
-    1) get de data
-
-
-inyection data in button: 
-        name,
-        apellido,
-
-status tlf: 'icon in button modificar'
-status agente : 'color button modificar'
-
-
+ 
 inyection data in card collapse:
         ext,
         in,
@@ -24,7 +14,7 @@ inyection data in card collapse:
 
 */
 
-// const $buttonCollapse = document.querySelector('#buttonCollapse')
+
 const $collapse = document.querySelector('#collapse')
 
 
@@ -50,7 +40,7 @@ function getDataUsers(pushData,pushCollapse){
     ];
 
  
-    collapse += '<div class="col collapseItems"><p><button class="btn btnCollapse widthCollapse '+pushData[i].actividadAgente+'" type="button" data-bs-toggle="collapse" data-bs-target="#'+pushData[i].name+pushData[i].id+'" aria-expanded="false" aria-controls="'+pushData[i].name+pushData[i].id+'">'+pushData[i].nombre+' '+pushData[i].apellido+'<span class=" icon '+pushData[i].actividadTelefono+'"></span></button></p><div class="row"><div class="col"><div class="collapse multi-collapse widthCollapse" id="'+pushData[i].name+pushData[i].id+'"><div class="card card-body">'+pushData[i].id+' Some placeholder content for the first collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.</div></div></div></div></div>'
+    collapse += '<div class="col collapseItems"><p><button class="btn btnCollapse widthCollapse '+pushData[i].actividadAgente+'" type="button" data-bs-toggle="collapse" data-bs-target="#'+pushData[i].name+pushData[i].id+'" aria-expanded="false" aria-controls="'+pushData[i].name+pushData[i].id+'">'+pushData[i].nombre+' '+pushData[i].apellido+'<span class=" icon '+pushData[i].actividadTelefono+'"></span></button></p><div class="row"><div class="col"><div class="collapse multi-collapse widthCollapse" id="'+pushData[i].name+pushData[i].id+'"><div class="card card-body column" style ="width: 250px; font-size:12px;"><div class="col row justify-content-between" style="width: initial;" ><p class="col align-self-start"><b>Nivel:</b>'+pushData[i].nivel+'</p><p class="col align-self-end"><b>Ext:</b>'+pushData[i].ext+'</p></div><div class="col row justify-content-between" style="width: initial;"><p class="col"><b>IN:</b>'+pushData[i].in+'</p><p class="col"><b>OUT:</b>'+pushData[i].out+'</p></div><p class="col"><b>Grupo:</b>'+pushData[i].grupo+'</p><p class="col"><b>Localización:</b>'+pushData[i].localizacion+'</p><p class="col"><b>Ultimo estado:</b>'+pushData[i].ultimoEstado+'</p>  </div></div></div></div></div>'
 
     
 
